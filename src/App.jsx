798d923f0fb3c86c8ae524ffe03092609332111b
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-10-14 17:10:04 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import MainSite from '../components/MainSite'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <MainSite>
+      </MainSite>
+    </>
+    
+  )
+}
+
+export default App
