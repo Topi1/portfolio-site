c26ca0583ef31fc41b7a07dec0593f295aceec0b
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-11-23 00:20:21 +0200

    3.5

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "../CSS/Footer.css"
+import logo2 from "../pictures/logo2.png"
+import github from "../icons/github.png"
 
 export default function Footer() {
   return (
     <footer className="footerMain">
+      <div className="footercontent">
+        <section className='footsec1'>
+          <h1>Thank you for visiting my site!</h1>
+          <h2>-Topi</h2>
+        </section>
 
+        <section className='footsec2'>
+          <img src={logo2}  className='footlogo'/>
+          <a className='footlink' href="">Topi1@GitHub <img src={github} className='giticon'/></a>
+        </section>
+      </div>
     </footer>
   )
 }
