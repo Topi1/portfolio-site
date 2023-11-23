f5746a70bf567e1efe9d108496f1f8b71fff1e5d
Topi Järvinen <topi.jarvinen1@gmail.com>
2023-11-23 18:08:52 +0200

    4.0

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
                     <li><a href='#about'>About me</a></li>
                     <li><a href='#resume'>Resume</a></li>
                     <li><a href='#portfolio'>Portfolio</a></li>
-                    <li><a href='"#contact'>Contact</a></li>
+                    <li><a href='#contact'>Contact</a></li>
                 </ul>
             </section>
         </section>
